e9f8e5e295d8eced104e42b8e57e66f4237c31e2
kaice88 <chinguyen2k1@gmail.com>
2023-03-04 00:23:08 +0700

    demo csv

--- demo.py
+++ demo.py
@@ -1,14 +1,17 @@
 import mediapipe as mp
 import os
 import numpy as np
+import pandas as pd
 import cv2
 from numpy.linalg import norm
-from createDecisionTree import createDecisionTree
+from BootsTrap import bootstrap
 
-input_dir = ["C:/Users/Lenovo/Desktop/KI6/PBL5/Train/CR",
-             "C:/Users/Lenovo/Desktop/KI6/PBL5/Train/WR"]
-output_dir = ["C:/Users/Lenovo/Desktop/KI6/PBL5/Train/RS0",
-              "C:/Users/Lenovo/Desktop/KI6/PBL5/Train/RS1"]
+
+input_dir = ["C:/Users/Lenovo/Desktop/KI6/PBL5/Train/Demo/CR",
+             "C:/Users/Lenovo/Desktop/KI6/PBL5/Train/Demo/WR1"]
+
+output_dir = ["C:/Users/Lenovo/Desktop/KI6/PBL5/Train/Demo/RS0",
+              "C:/Users/Lenovo/Desktop/KI6/PBL5/Train/Demo/RS1"]
 
 # khởi tạo thư viện mediapipe
 mpPose = mp.solutions.pose
@@ -18,17 +21,22 @@ mpDraw = mp.solutions.drawing_utils  # vẽ khung xương trên ảnh
 
 # lưu thông số từng điểm trên khung xương
 numberOfFiles = len(os.listdir(input_dir[0]))
-lm_list = np.empty((2, numberOfFiles, 45))
-label = [0, 1]
+
+
+# 2 class; số ảnh train mỗi class ;lấy 15 điểm mỗi điểm lấy x,y nên 15*2 = 30;
+lm_list = np.empty((2, numberOfFiles, 30))
+label_list = ["CR", "WR"]
 
 
 def make_landmark_timestep(results, img):
-    c_lm = np.empty((15, 3), dtype="float")
+
+    # lấy 15 điểm mỗi điểm lấy x,y
+    c_lm = np.empty((15, 2), dtype="float")
     for id, lm in enumerate(results.pose_landmarks.landmark):
         if (id < 15):
             c_lm[id, 0] = lm.x
             c_lm[id, 1] = lm.y
-            c_lm[id, 2] = lm.z
+            # c_lm[id, 2] = lm.z
     return c_lm.flatten()
 
 
@@ -64,64 +72,52 @@ def processImage(label, id, image_path1, image_path2):
             # vẽ lên ảnh
             image1 = draw_landmark_on_image(mpDraw, image, results)
             # image2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            cv2.imwrite(
-                image_path2, image1)
+            cv2.imwrite(image_path2, image1)
 
 
-# chuẩn hóa phạm vi [0,1]
-def normalize(lm):
-    r, c = lm.shape
-    temp = np.empty((r, c), dtype="float")
-    maxX = max(lm[:, 0])
-    maxY = max(lm[:, 1])
-    maxZ = max(abs(lm[:, 2]))
-    for i in range(r):
-        temp[i, 0] = lm[i, 0]/maxX
-        temp[i, 1] = lm[i, 1]/maxY
-        temp[i, 2] = lm[i, 2]/maxZ
-    return temp
+#  chuẩn hóa phạm vi [0,1]
+# def normalize(cm):
+#     r, c = cm.shape
+#     temp = np.empty((r, c), dtype="float")
+#     maxX = max(cm[:, 0])
+#     maxY = max(cm[:, 1])
+#     for i in range(r):
+#         temp[i, 0] = cm[i, 0]/maxX
+#         temp[i, 1] = cm[i, 1]/maxY
+#     return temp
 
 
-for i in label:
+# trích xuất vector đặc trưng và lưu vào file csv
+for i, label in enumerate(label_list):
     for id, image in enumerate(os.listdir(input_dir[i])):
         processImage(
             i, id, f"{input_dir[i]}/{image}", f"{output_dir[i]}/{image}")
+    # Lưu vào file csv
+    df = pd.DataFrame(lm_list[i, :, :])
+    df.to_csv(label + ".txt")
 
 
-# print(lm_list[14, :, :])
-# processImage(0, "C:/Users/Lenovo/Desktop/KI6/PBL5/IMG1.jpg",
-#              "C:/Users/Lenovo/Desktop/KI6/PBL5/IMG2.jpg")
-# processImage(0, "C:/Users/Lenovo/Desktop/KI6/PBL5/IMGTH2.jpg",
-#              "C:/Users/Lenovo/Desktop/KI6/PBL5/IMGLM2.jpg")
+# đọc dữ liệu từ file csv
+
+# CR_dataframe = pd.read_csv("CR.txt").iloc[:, 1:].values
+# WR_dataframe = pd.read_csv("WR.txt").iloc[:, 1:].values
+
+# sau khi lưu được vào file csv thì train data trong file RandomForest.py
 
 
 # ----------------------------------------Random forest---------------------------------------------
 
 
-M = 10
-n = 40
+# M = 10
+# n = 40
 
+# CR = bootstrap(lm_list[0, :, :], n, M)
+# WR = bootstrap(lm_list[1, :, :], n, M)
+# label_vector = np.concatenate((np.full((40), 0), np.full((40), 1)))
 
-def bootstrap(input, n, M):
-    d2, d1 = np.shape(input)
-    indexesOf1dArray = np.random.choice(
-        np.arange(d2), size=(M, n), replace=True)  # 2d array
-
-    results = np.empty((M, n, d1))
-    for i in range(M):
-        for j in range(n):
-            indexOf1d = indexesOf1dArray[i, j]
-            results[i, j, :] = input[indexOf1d, :]
-    return results
-
-
-CR = bootstrap(lm_list[0, :, :], n, M)
-WR = bootstrap(lm_list[1, :, :], n, M)
-label_list = np.concatenate((np.full((40), 0), np.full((40), 1)))
-
-for i in range(10):
-    data = np.concatenate((CR[i, :, :], WR[i, :, :]))
-    createDecisionTree(data, label_list)
+# for i in range(10):
+#     data = np.concatenate((CR[i, :, :], WR[i, :, :]))
+#     trainModel(data, label_vector)
 
 
 # --------------------------------------------------------------------------------------------------
